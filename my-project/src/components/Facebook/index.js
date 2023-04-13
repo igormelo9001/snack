060569c090b296b8d20cf4b0a5631b60710fc615
unknown <igormelo9001@gmail.com>
2023-04-13 18:56:24 -0300

    update facebook component

--- my-project/src/components/Facebook/index.js
+++ my-project/src/components/Facebook/index.js
@@ -1,27 +1,27 @@
 import React, { useState, useEffect } from "react"
 import {Box, FlatList} from 'native-base'
 import axios from "axios"
+import { ListItem, Avatar } from 'react-native-elements';
 
-const YOUR_ACCESS_TOKEN = "134963599425482"
+const YOUR_ACCESS_TOKEN = "1c33ae0c2498354a2ea6b6d1b9449cb4"
 
+const API_ACCESS = f2d112bb7cc3ee2e3453ad01955b207e
 
 const UserList = () => {
-    const [users, setUsers] = useState([]);
-  
-    useEffect(() => {
-      const fetchUsers = async () => {
-        const response = await axios.get(
-          `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,thumbnail_url,permalink&access_token=${YOUR_ACCESS_TOKEN}`
-        );
-        setUsers(response.data.data);
-      };
-  
-      fetchUsers();
-    }, []);
-    
-}
+  const [users, setUsers] = useState([]);
 
-const renderItem = ({ item }) => (
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await axios.get(
+        `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,thumbnail_url,permalink&access_token=${YOUR_ACCESS_TOKEN}`
+      );
+      setUsers(response.data.data);
+    };
+
+    fetchUsers();
+  }, []);
+
+  const renderItem = ({ item }) => (
     <ListItem bottomDivider>
       <Avatar source={{ uri: item.media_url }} />
       <ListItem.Content>
@@ -31,16 +31,13 @@ const renderItem = ({ item }) => (
     </ListItem>
   );
 
-const Facebook = () => {
-        return(
-            <Box flex={1} backgroundColor="#f8f8f8" flexDirection="column" paddingX={4}>
-                 <FlatList 
-                  data={UserList}
-                  renderItem={renderItem}
-                  showsHorizontalScrollIndicator={false}
-                  />
-            </Box>
-        )
-}
+  return (
+    <FlatList
+      data={users}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}
+    />
+  );
+};
 
-export default Facebook
+export default UserList;
